3c3dee74133d9454fff3591c36219215e707c790
neowyp <wypsmall@126.com>
2015-06-08 12:17:57 +0800

    refactor code ctrl+alt+m

--- im-user/src/main/java/com/neo/ce/user/utils/UGuava.java
+++ im-user/src/main/java/com/neo/ce/user/utils/UGuava.java
@@ -20,55 +20,79 @@ public class UGuava {
             System.out.println("isNullorEmpty : " + isNullorEmpty);
 
             //获取两个字符串相同的前缀和后缀
-            String a = "com.gome.user";
-            String b = "com.gome.account";
-            String prefix = Strings.commonPrefix(a,b);
-            System.out.println(prefix);
-
-            String s1 = "test.jsp";
-            String s2 = "img.jsp";
-            String suffix = Strings.commonSuffix(s1, s2);
-            System.out.println(suffix);
+            getPrefixSuffix();
 
             //左右不全字符 【这个很有用】
-            int strlen = 10;
-            String padEnd = Strings.padEnd("" + 1234, strlen, '0');
-            System.out.println(padEnd);
+            padRightLeft();
 
-            String padStart = Strings.padStart("" + 432, strlen, '0');
-            System.out.println(padStart);
+            //Splitter拆分字符床
+            splitterString();
 
-            //Splittler拆分字符床
-            Iterable<String> splitResults = Splitter.onPattern("[,，]{1,}")
-                    .trimResults()
-                    .omitEmptyStrings()
-                    .split("hello,word,,世界，水平");
-            for(String item : splitResults) {
-                System.out.println(item);
-            }
-
-            //Splittler二次拆分
-            String toSplitString = "a=b;c=d,e=f";
-            Map<String,String> kvs = Splitter.onPattern("[,;]{1,}").withKeyValueSeparator('=').split(toSplitString);
-            for (Map.Entry<String,String> entry : kvs.entrySet()) {
-                System.out.println(String.format("%s=%s", entry.getKey(),entry.getValue()));
-            }
+            //Splitter二次拆分
+            splitterSecString();
 
             //字符串合并
-            String joinStr = Joiner.on(" ").join(new String[] {"hello", "my", "world"});
-            System.out.println(joinStr);
+            joinString();
 
             //合并map
-            Map<String, String> map = new HashMap<String, String>();
-            map.put("ip", "10.10.0.123");
-            map.put("port", "8090");
-            String mapStr = Joiner.on("\r\n").withKeyValueSeparator("=").join(map);
-            System.out.println(mapStr);
+            joinMap();
 
 
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+    }
+
+    private static void padRightLeft() {
+        int strlen = 10;
+        String padEnd = Strings.padEnd("" + 1234, strlen, '0');
+        System.out.println(padEnd);
+
+        String padStart = Strings.padStart("" + 432, strlen, '0');
+        System.out.println(padStart);
+    }
+
+    private static void getPrefixSuffix() {
+        String a = "com.gome.user";
+        String b = "com.gome.account";
+        String prefix = Strings.commonPrefix(a, b);
+        System.out.println(prefix);
+
+        String s1 = "test.jsp";
+        String s2 = "img.jsp";
+        String suffix = Strings.commonSuffix(s1, s2);
+        System.out.println(suffix);
+    }
+
+    private static void splitterSecString() {
+        String toSplitString = "a=b;c=d,e=f";
+        Map<String,String> kvs = Splitter.onPattern("[,;]{1,}").withKeyValueSeparator('=').split(toSplitString);
+        for (Map.Entry<String,String> entry : kvs.entrySet()) {
+            System.out.println(String.format("%s=%s", entry.getKey(),entry.getValue()));
+        }
+    }
+
+    private static void splitterString() {
+        Iterable<String> splitResults = Splitter.onPattern("[,，]{1,}")
+                .trimResults()
+                .omitEmptyStrings()
+                .split("hello,word,,世界，水平");
+        for(String item : splitResults) {
+            System.out.println(item);
+        }
+    }
+
+    private static void joinString() {
+        String joinStr = Joiner.on(" ").join(new String[] {"hello", "my", "world"});
+        System.out.println(joinStr);
+    }
+
+    private static void joinMap() {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("ip", "10.10.0.123");
+        map.put("port", "8090");
+        String mapStr = Joiner.on("\r\n").withKeyValueSeparator("=").join(map);
+        System.out.println(mapStr);
     }
 }
